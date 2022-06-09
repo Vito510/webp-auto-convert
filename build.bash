1d5693e01d78b9d52ceec1965dc711097549c0b5
Vito Čuić <cuicvito@gmail.com>
2022-06-09 21:25:19 +0200

    Added option to hide console, added output format to config

--- build.bash
+++ build.bash
@@ -1,2 +1,2 @@
-pyinstaller.exe ./webp-auto-convert.py --onefile --icon=./icon.ico
+pyinstaller.exe --noconsole ./webp-auto-convert.py --onefile --icon=./icon.ico
 cp config.txt ./dist/config.txt
--- webp-auto-convert.py
+++ webp-auto-convert.py
@@ -1,19 +1,15 @@
 import os
 import sys
 import json
+import subprocess
+
+CREATE_NO_WINDOW = 0x08000000
 
 def main():
-    print("WebP Auto Convert")
 
-    print("Usage: webp-auto-convert.exe <file>")
-
-    x = input("Press enter to close")
-
-def start():
-
-    if len(sys.argv) == 1:
-        main()
-        return None
+    if len(sys.argv) != 2:
+        raise Exception("No input file specified")
+        sys.exit(0)
 
     path = sys.argv[0].replace('webp-auto-convert.exe', '')
     fpath = sys.argv[1]
@@ -21,13 +17,23 @@ def start():
 
     config = json.load(open(path + 'config.txt'))
 
-    print(sys.argv)
+    output_format = '.'+config['output-format']
 
-    os.system(f'ffmpeg.exe -y -i "{fpath}" "{fpath.replace("."+fext,".png")}"')
+    c = f'ffmpeg.exe -y -i "{fpath}" "{fpath.replace("."+fext, output_format)}" -hide_banner'
+
+    if config['hideconsole']:
+        subprocess.call(c, creationflags=CREATE_NO_WINDOW)
+    else:
+        os.system(c)
 
     if config['remove-on-completion']: os.remove(fpath)
-    if config['open-on-completion']: os.system(f'explorer "{fpath.replace("."+fext,".png")}"')
+    if config['open-on-completion']: 
+        c = f'explorer.exe "{fpath.replace("."+fext, output_format)}"'
+        if config['hideconsole']:
+            subprocess.call(c, creationflags=CREATE_NO_WINDOW)
+        else:
+            os.system(c)
 
     sys.exit(0)
 
-start()
+main()
